7f487f7c8c24ab070704b776c1b30bd90df6ecca
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-05 11:58:47 +0200

    Update gaz_vert_10_structure.sql

--- sql/gaz_vert_10_structure.sql
+++ sql/gaz_vert_10_structure.sql
@@ -22,94 +22,153 @@ COMMENT ON SCHEMA met_env IS 'Schéma pour les données métiers sur l''environn
 ------------------------------------------------------------------------
 -- Table: met_env.m_env_gaz_vert_au_r6_d1_unites_metha
 
--- DROP TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha;
-CREATE TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha (
+--DROP TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo;
+CREATE TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo (
 	id serial NOT NULL,
-	id_metha varchar(8),
-	scenario varchar(10),
-	canton varchar(4),
-	annee_cible numeric,
-	taille_unite varchar(4),
-	taille_unite_libelle varchar(4),
-	injection boolean default false,
-	cogeneration boolean DEFAULT false,
-	power2gas boolean DEFAULT false,
-	niveau varchar(10),
-	techno varchar(50),
-	levee_contrainte varchar(100),
-  	commentaires text,
-	annee_donnees varchar(4),
-	date_import date,
-	date_maj date,
-  	geom_valide  boolean DEFAULT false,
-  	geom geometry(POINT, 2154),
-	CONSTRAINT m_env_gaz_vert_au_r6_d1_unites_metha_pkey PRIMARY KEY (id),
-  	CONSTRAINT m_env_gaz_vert_au_r6_d1_unites_metha_uniq UNIQUE (id_metha)
+	id_metha varchar(8) NULL,
+	scenario varchar(10) NULL,
+	canton varchar(4) NULL,
+	annee_cible numeric NULL,
+	taille_unite varchar(4) NULL,
+	taille_unite_libelle varchar(150) NULL,
+	injection bool NULL DEFAULT false,
+	cogeneration bool NULL DEFAULT false,
+	power2gas bool NULL DEFAULT false,
+	niveau varchar(10) NULL,
+	techno varchar(50) NULL,
+	levee_contrainte varchar(100) NULL,
+	commentaires text NULL,
+	annee_donnees varchar(4) NULL,
+	date_import date NULL,
+	date_maj date NULL,
+	geom_valide bool NULL DEFAULT false,
+	geom geometry(POINT, 2154) NULL,
+	CONSTRAINT m_env_gaz_vert_au_r6_d1_unites_metha_geo_pkey PRIMARY KEY (id),
+	CONSTRAINT m_env_gaz_vert_au_r6_d1_unites_metha_geo_uniq UNIQUE (id_metha)
 );
 
 -- 
-COMMENT ON TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha IS 'Resau de transport du gaz vert en Nouvelle-Aquitaine';
+COMMENT ON TABLE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo IS 'Resau de transport du gaz vert en Nouvelle-Aquitaine';
 
 --
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.id IS 'Identifiant';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.id_metha IS 'Identifiant de l''unité de méthanisation';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.scenario IS 'Scénario';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.canton IS 'Canon d''appartenance';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.annee_cible IS 'Année de mise en oeuvre';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.taille_unite IS 'Taille de l''unité';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.taille_unite_libelle IS 'Libellé de la taille de l''unité';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.injection IS 'Unité en injection';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.cogeneration IS 'Unité cogénératrice';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.power2gas IS 'Convertion éléctrique en gaz';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.niveau IS 'Niveau';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.techno IS 'Technologie';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.levee_contrainte IS 'Levée de contrainte';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.commentaires IS 'Commentaires';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.annee_donnees IS 'Année de la données pour l''historisation';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.date_import IS 'Date d''import de la donnée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.date_maj IS 'Date de mise à jour de la donnée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.geom_valide IS 'Géométrie validée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha.geom IS 'Géométrie point';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.id IS 'Identifiant';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.id_metha IS 'Identifiant de l''unité de méthanisation';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.scenario IS 'Scénario';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.canton IS 'Canon d''appartenance';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.annee_cible IS 'Année de mise en oeuvre';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.taille_unite IS 'Taille de l''unité';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.taille_unite_libelle IS 'Libellé de la taille de l''unité';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.injection IS 'Unité en injection';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.cogeneration IS 'Unité cogénératrice';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.power2gas IS 'Convertion éléctrique en gaz';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.niveau IS 'Niveau';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.techno IS 'Technologie';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.levee_contrainte IS 'Levée de contrainte';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.commentaires IS 'Commentaires';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.annee_donnees IS 'Année de la données pour l''historisation';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.date_import IS 'Date d''import de la donnée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.date_maj IS 'Date de mise à jour de la donnée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.geom_valide IS 'Géométrie validée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo.geom IS 'Géométrie point';
 
 --
-CREATE INDEX sidx_m_env_gaz_vert_au_r6_d1_unites_metha_geom ON met_env.m_env_gaz_vert_au_r6_d1_unites_metha USING gist (geom);
+CREATE INDEX sidx_m_env_gaz_vert_au_r6_d1_unites_metha_geo_geom ON met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo USING gist (geom);
+
+-- Ajout de données
+INSERT INTO met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo (
+id_metha, scenario, canton, annee_cible, taille_unite, taille_unite_libelle, injection, cogeneration, power2gas, 
+niveau, techno, levee_contrainte, commentaires, annee_donnees, date_import, date_maj, geom_valide, geom) 
+SELECT 
+id_metha, scenario, canton, anneecible, tailleunit, null, cast(injection as boolean), cast(coge as boolean), cast(power2gas as boolean), 
+niveau, techno, leveecontr, null, '2020', now(), null, false, geom 
+FROM z_maj.au_r6_d1_unites_metha;
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='Moins de 80 m3/h'
+WHERE taille_unite in ('U1','U2','U3','U4');
 
 
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='80 m3/h'
+WHERE taille_unite = 'U5';
 
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='160 m3/h'
+WHERE taille_unite = 'U6';
 
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='270 m3/h'
+WHERE taille_unite = 'U7';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='380 m3/h'
+WHERE taille_unite = 'U8';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='490 m3/h'
+WHERE taille_unite = 'U9';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='600 m3/h'
+WHERE taille_unite = 'U10';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='710 m3/h'
+WHERE taille_unite = 'U11';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='810 m3/h'
+WHERE taille_unite = 'U12';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='920 m3/h'
+WHERE taille_unite = 'U13';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='1030 m3/h'
+WHERE taille_unite = 'U14';
+
+UPDATE met_env.m_env_gaz_vert_au_r6_d1_unites_metha_geo 
+SET taille_unite_libelle='1190 m3/h'
+WHERE taille_unite = 'U15';
 
 ------------------------------------------------------------------------
--- Table: met_env.m_env_gaz_vert_au_r6_d1_reseau_transport
+-- Table: met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo
 
--- DROP TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport;
-CREATE TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport (
+--DROP TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo;
+CREATE TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo (
 	id serial NOT NULL,
-	scenario varchar(10),
-  	commentaires text,
-	annee_donnees varchar(4),
-	date_import date,
-	date_maj date,
-  	geom_valide  boolean DEFAULT false,
-  	geom geometry(MULTIPOLYGON, 2154),
-	CONSTRAINT m_env_gaz_vert_au_r6_d1_reseau_transport_pkey PRIMARY KEY (id)
+	scenario varchar(10) NULL,
+	commentaires text NULL,
+	annee_donnees varchar(4) NULL,
+	date_import date NULL,
+	date_maj date NULL,
+	geom_valide bool NULL DEFAULT false,
+	geom geometry(MULTIPOLYGON, 2154) NULL,
+	CONSTRAINT m_env_gaz_vert_au_r6_d1_reseau_transport_geo_pkey PRIMARY KEY (id)
 );
 
 -- 
-COMMENT ON TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport IS 'Reseau de transport du gaz vert en Nouvelle-Aquitaine';
+COMMENT ON TABLE met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo IS 'Reseau de transport du gaz vert en Nouvelle-Aquitaine';
 
 --
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.id IS 'Identifiant';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.scenario IS 'Scénario';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.commentaires IS 'Commentaires';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.annee_donnees IS 'Année de la données pour l''historisation';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.date_import IS 'Date d''import de la donnée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.date_maj IS 'Date de mise à jour de la donnée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.geom_valide IS 'Géométrie validée';
-COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport.geom IS 'Géométrie polygone';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.id IS 'Identifiant';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.scenario IS 'Scénario';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.commentaires IS 'Commentaires';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.annee_donnees IS 'Année de la données pour l''historisation';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.date_import IS 'Date d''import de la donnée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.date_maj IS 'Date de mise à jour de la donnée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.geom_valide IS 'Géométrie validée';
+COMMENT ON COLUMN met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo.geom IS 'Géométrie polygone';
 
 --
-CREATE INDEX sidx_m_env_gaz_vert_au_r6_d1_reseau_transport_geom ON met_env.m_env_gaz_vert_au_r6_d1_reseau_transport USING gist (geom);
+CREATE INDEX sidx_m_env_gaz_vert_au_r6_d1_reseau_transport_geo_geom ON met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo USING gist (geom);
 
+-- Ajout de données
+INSERT INTO met_env.m_env_gaz_vert_au_r6_d1_reseau_transport_geo (
+scenario, commentaires, annee_donnees, date_import, date_maj, geom_valide, geom) 
+SELECT scenario, null, '2020', now(), null, false, geom
+FROM z_maj.au_r6_d1_reseau_transport;
 
 ------------------------------------------------------------------------
 -- Table: met_env.m_env_gaz_vert_au_r6_d1_rebours_crees
