86af31d2750aaf06c263bf9d1dd992adb6e95e26
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-05 09:19:53 +0200

    Create gaz_vert_00_trace.sql

---
+++ sql/gaz_vert_00_trace.sql
@@ -0,0 +1,13 @@
+/*GAZ VERT V1.0*/
+/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
+/* gaz_vert_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT*/
+
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-06-04 : TV / initialisation du code uniquement
+2020-06-04 : TV / initialisation de la structure de la base de données sur le gaz vert
