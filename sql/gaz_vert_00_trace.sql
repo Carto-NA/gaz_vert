29aaaf129887c30fe157465a21ff356e309c5103
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-11 14:20:41 +0200

    Communes raccordées et canton ressources utilisées

--- sql/gaz_vert_00_trace.sql
+++ sql/gaz_vert_00_trace.sql
@@ -11,3 +11,5 @@
 #################################################################### SUIVI CODE SQL ####################################################################
 2020-06-04 : TV / initialisation du code uniquement
 2020-06-04 : TV / initialisation de la structure de la base de données sur le gaz vert
+2020-06-11 : TV / ajout de la structures et des données sur les communes raccordés
+2020-06-11 : TV / ajout de la structure et des données sur les cantons et le type de ressource utilisée
